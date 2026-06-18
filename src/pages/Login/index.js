19df37998ec82f2a2fb9f5598003b5d487322f43
XuCai021130 <xucai7140@gmail.com>
2026-06-18 19:46:06 -0400

    login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,53 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
 const Login = () => {
-  return <h1>This is login page</h1>
+  const onFinish = ({ password, phone}) => {
+    console.log(password);
+    
+  }
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form
+          validateTrigger="onBlur"
+          onFinish={onFinish}>
+          <Form.Item
+            name='phone'
+            rules={[
+              {
+              required: true,
+              message: 'Please input your phone number'
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: 'Please enter correct phone number'
+              }
+            ]}>
+            <Input size="large" placeholder="Please enter your phone number" />
+          </Form.Item>
+          <Form.Item
+            name='password'
+            rules={[
+              {
+              required: true,
+              message: 'Please input your password'
+              }
+            ]}>
+            <Input size="large" placeholder="Please enter your password" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+import { request } from "node_modules/axios/index.cjs";
+
+export { request }
